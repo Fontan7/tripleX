6c2928aad239041a849ba1b88d6b7b07d2a7a897
Fontän <nahuelfontan96@gmail.com>
2020-06-29 23:00:30 -0300

    variables video finished, experimenting with stringPrinter

--- main/triplex.cpp
+++ main/triplex.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <windows.h>
+using std::string;
 
 int main()
 {
     //namespace :: property
-    std::cout << "\n Hello World! \n" ; 
+    std::cout << "\n Attention you are entering a restricted zone \n" ; 
+    std::cout << "\n Enter the correct code in order to continue \n" ; 
+
+    int a = 1;
+    int b = 2;
+    int c = 3;
+
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+
+    return 0;
+}
+
+int stringPrinter (string phrase) 
+{
+    int strLength = phrase.length();
+
     return 0;
 }
